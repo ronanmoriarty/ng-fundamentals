4cdb8a60a07f997c04644870971c7a9f1fd3626a
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-12-01 17:02:02 +0000

    Undo previous commit (keeping previous commit for reference)

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -7,8 +7,7 @@ import { Component } from "@angular/core";
         <h1>Upcoming Angular Events</h1>
         <hr/>
         <event-thumbnail
-            [event]="event1"
-            (eventClick)="handleEventClicked($event)">
+            [event]="event1">
         </event-thumbnail>
     </div>
     `
@@ -27,8 +26,4 @@ export class EventsListComponent {
             country: 'England'
         }
     };
-
-    handleEventClicked(data){
-        console.log('received: ' + data);
-    }
 }
