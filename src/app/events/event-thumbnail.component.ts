3e3d4f3a307b09bf8c5e158dfddad080a8a4dd42
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-12-01 17:34:05 +0000

    Revert "Exploring how global styles and component styles work"
    This reverts commit d760e86277c0b586759555e536319a9de77d448a.

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -16,6 +16,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     styles: [
         `
         .pad-left { padding-left: 10px; }
+        .well div { color: #bbb; }
         `
     ]
 })
