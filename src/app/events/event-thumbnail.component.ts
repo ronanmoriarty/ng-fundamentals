6ba166f97c49e34f811ea933c232b0eb931f369d
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-12-01 17:15:53 +0000

    Add styles to component to put spacing between address and city

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -10,10 +10,12 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
         <div>Price: \${{event.price}}</div>
         <div>
             <span>Location: {{event.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
         </div>
-    </div>`
+    </div>`,
+    styles: [
+        '.pad-left { padding-left: 10px; }'
+    ]
 })
 export class EventThumbnailComponent {
     @Input() event:any;
